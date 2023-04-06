d29c600e4cb427dab5e53ebdb55364bfcfba7894
jervi <gacembekhira@gmail.com>
2023-04-06 03:25:35 +0000

    added api endpoit page

---
+++ database/migrations/2023_04_06_032147_create_endpoints_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('endpoints', function (Blueprint $table) {
+            $table->id();
+            $table->string('URL');
+            $table->string('Method');
+            $table->string('parameters')->nullable();
+            $table->boolean('requireAuth');
+            $table->string('response')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('endpoints');
+    }
+};
